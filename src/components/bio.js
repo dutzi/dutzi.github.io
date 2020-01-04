bdc71e66e0f4e8cfa10316671c5e99c614ad5aa7
Eldad <eldad@spot.im>
2020-01-04 06:19:07 +0200

    Revert "Updates"
    This reverts commit dd77d4ebd7730d3ee6891400222651714fae25ed.

---
+++ src/components/bio.js
@@ -0,0 +1,71 @@
+/**
+ * Bio component that queries for data
+ * with Gatsby's useStaticQuery component
+ *
+ * See: https://www.gatsbyjs.org/docs/use-static-query/
+ */
+
+import React from 'react'
+import { useStaticQuery, graphql } from 'gatsby'
+import Image from 'gatsby-image'
+
+import { rhythm } from '../utils/typography'
+
+const Bio = ({ greeting }) => {
+  const data = useStaticQuery(graphql`
+    query BioQuery {
+      avatar: file(absolutePath: { regex: "/profile-pic.png/" }) {
+        childImageSharp {
+          fixed(width: 50, height: 50) {
+            ...GatsbyImageSharpFixed
+          }
+        }
+      }
+      site {
+        siteMetadata {
+          author
+          social {
+            twitter
+          }
+        }
+      }
+    }
+  `)
+
+  const { author } = data.site.siteMetadata
+  return (
+    <div
+      style={{
+        display: `flex`,
+        marginBottom: rhythm(2.5),
+        alignItems: 'center',
+      }}
+    >
+      <Image
+        fixed={data.avatar.childImageSharp.fixed}
+        alt={author}
+        style={{
+          marginRight: rhythm(1 / 2),
+          marginBottom: 0,
+          minWidth: 50,
+          borderRadius: `100%`,
+        }}
+        imgStyle={{
+          borderRadius: `50%`,
+        }}
+      />
+      {greeting ? (
+        <p style={{ margin: '0' }}>
+          Hey, I'm <strong>{author}!</strong>
+        </p>
+      ) : (
+        <p style={{ margin: '0' }}>
+          Thanks for reading!
+          <br />— <strong>{author}</strong>
+        </p>
+      )}
+    </div>
+  )
+}
+
+export default Bio
