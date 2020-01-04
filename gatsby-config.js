856784f7062cf37649fa5542c42087ff199d2df0
Eldad <eldad@spot.im>
2020-01-04 09:10:10 +0200

    add google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -50,7 +50,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: `UA-7442821-15`,
       },
     },
     `gatsby-plugin-feed`,
