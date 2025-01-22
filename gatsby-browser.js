33c7040e997c982f949f7b8537056b22e2a11e9d
Eldad Bercovici <eldad@deepdub.ai>
2025-01-22 09:50:51 +0200

    add keren branch

--- gatsby-browser.js
+++
@@ -1,8 +0,0 @@
-// custom typefaces
-// import 'typeface-montserrat'
-// import 'typeface-merriweather'
-import 'typeface-fira-mono'
-// require("prismjs/themes/prism-solarizedlight.css")
-require('./static/prism-theme.css')
-require('./static/prism-theme-dark.scss')
-require('./static/my-theme-changes.css')
